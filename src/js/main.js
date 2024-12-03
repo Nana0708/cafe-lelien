13a09ab223f70b36a03f1cc691fea3818b57bbdb
nana kashiwagi <nana30708@gmail.com>
2024-12-03 23:46:56 +0900

    アニメーションスライドイン完成

--- src/js/main.js
+++ src/js/main.js
@@ -14,20 +14,33 @@
 //     delay: 1.5,
 //   });
 
-
 // -------------- ハンバーガー --------------//
+// -------------- 各ページkvのアニメーション --------------//
+
+  gsap.from(".js_m_kv__copy", {
+    opacity: 0,            
+    y: 50,                 
+    scale: 0.8,            
+    duration: 0.7,         
+    ease: "power2.out",    
+    scrollTrigger: {
+      trigger: ".js_m_kv__copy", 
+      start: "top 80%",      
+    }
+  });
 
 // -------------- スライドイン --------------//
 // top
+// concept
 gsap.from(".js_top-concept",{
-    y:200,
+    y:50,
     autoAlpha: 0,
     duration: 1,
     ease: "Power4.inOut",
     
 scrollTrigger: {
     trigger: ".js_top-concept__trigger",
-    start: "top center",
+    start: "top 80%",
   
     markers: true,
   },
@@ -38,13 +51,184 @@ scrollTrigger: {
   },
 });
 
+// menu
+gsap.from(".js_top-menu",{
+    y:50,
+    autoAlpha: 0,
+    duration: 1,
+    ease: "Power4.inOut",
+    
+scrollTrigger: {
+    trigger: ".js_top-menu__trigger",
+    start: "top 80%",
+  
+    markers: true,
+  },
+  stagger: {
+    each: 0.6,
+    from: "start",
+  },
+});
+
+// news
+gsap.from(".js_top-news",{
+    y:50,
+    autoAlpha: 0,
+    duration: 1,
+    ease: "Power4.inOut",
+    
+scrollTrigger: {
+    trigger: ".js_top-news__trigger",
+    start: "top 80%",
+  
+    markers: true,
+  },
+  stagger: {
+    each: 0.6,
+    from: "start",
+  },
+});
+
+// access
+gsap.from(".js_top-access",{
+    y:50,
+    autoAlpha: 0,
+    duration: 1,
+    ease: "Power4.inOut",
+    
+scrollTrigger: {
+    trigger: ".js_top-access__trigger",
+    start: "top 80%",
+  
+    markers: true,
+  },
+  stagger: {
+    each: 0.6,
+    from: "start",
+  },
+});
+
+// contact
+gsap.from(".js_top-contact",{
+    y:200,
+    autoAlpha: 0,
+    duration: 1,
+    ease: "Power4.inOut",
+    
+scrollTrigger: {
+    trigger: ".js_top-contact__trigger",
+    start: "top center",
+  
+    markers: true,
+  },
+  stagger: {
+    each: 0.6,
+    from: "start",
+  },
+});
+
 
 
 
 // menu
+// drink
+gsap.from(".js_menu_drink",{
+    y:50,
+    autoAlpha: 0,
+    duration: 1,
+    ease: "Power4.inOut",
+    
+scrollTrigger: {
+    trigger: ".js_menu_drink__trigger",
+    start: "top 80%",
+  
+    markers: true,
+  },
+  stagger: {
+    each: 0.6,
+    from: "start",
+  },
+});
+
+// food
+gsap.from(".js_menu_food",{
+    y:50,
+    autoAlpha: 0,
+    duration: 1,
+    ease: "Power4.inOut",
+    
+scrollTrigger: {
+    trigger: ".js_menu_food__trigger",
+    start: "top 80%",
+  
+    markers: true,
+  },
+  stagger: {
+    each: 0.6,
+    from: "start",
+  },
+});
+
+
+
 // news
+gsap.from(".js_news",{
+    y:50,
+    autoAlpha: 0,
+    duration: 1,
+    ease: "Power4.inOut",
+    
+scrollTrigger: {
+    trigger: ".js_news_trigger",
+    start: "top 80%",
+  
+    markers: true,
+  },
+  stagger: {
+    each: 0.6,
+    from: "start",
+  },
+});
+
 // single
+gsap.from(".js_single",{
+    y:50,
+    autoAlpha: 0,
+    duration: 1,
+    ease: "Power4.inOut",
+    
+scrollTrigger: {
+    trigger: ".js_single__trigger",
+    start: "top 80%",
+  
+    markers: true,
+  },
+  stagger: {
+    each: 0.6,
+    from: "start",
+  },
+});
+
+
 // contact
+gsap.from(".js_contact",{
+    y:50,
+    autoAlpha: 0,
+    duration: 1,
+    ease: "Power4.inOut",
+    
+scrollTrigger: {
+    trigger: ".js_contact__trigger",
+    start: "top 80%",
+  
+    markers: true,
+  },
+  stagger: {
+    each: 0.6,
+    from: "start",
+  },
+});
+
 
 
 // -------------- ハンバーガー --------------//
